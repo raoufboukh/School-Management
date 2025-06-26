7718021e689db78f599c5c6c92bb44658f2c508a
raoufboukh <zakib2316@gmail.com>
2025-06-26 16:11:27 +0100

    refactor layout and styling: enhance globals.css with new theme colors and update layoutWrapper

--- client/components/layoutWrapper.tsx
+++ client/components/layoutWrapper.tsx
@@ -1,12 +1,29 @@
+"use client";
 import { store } from "@/redux/store/store";
 import { Provider } from "react-redux";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "./ui/sidebar";
+import { AppSidebar } from "./Sidebar/AppSidebar";
+import Navbar from "./Navbar/Navbar";
 
 const LayoutWrapper = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="px-3 flex items-center gap-4 shadow-sm border-b border-gray-200">
+            <SidebarTrigger />
+            <Navbar />
+          </header>
+          <main>{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </Provider>
+  );
 };
 
 export default LayoutWrapper;
